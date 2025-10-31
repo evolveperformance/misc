0a2d325fd64406e045be38aba3a4fbc0739abdd5
Luca Strussione <lstru001@cs.hs-rm.de>
2025-10-31 16:31:32 +0100

    add automatic temp folder clear

--- EvolveOS.bat
+++ EvolveOS.bat
@@ -1160,6 +1160,23 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" /v "DODo
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" /v "SearchOrderConfig" /t REG_DWORD /d "0" /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" /v "DontSearchWindowsUpdate" /t REG_DWORD /d "1" /f >nul 2>&1
 
+@echo Create Scheduled Task for Weekly TEMP Cleanup
+:: Create batch file in Misc folder
+(
+echo @echo off
+echo del /q /f /s "C:\Windows\Temp\*" ^>nul 2^>^&1
+echo for /d %%%%d in ("C:\Windows\Temp\*"^) do rd /s /q "%%%%d" ^>nul 2^>^&1
+echo for /d %%%%u in ("C:\Users\*"^) do ^(
+echo     if exist "%%%%u\AppData\Local\Temp" ^(
+echo         del /q /f /s "%%%%u\AppData\Local\Temp\*" ^>nul 2^>^&1
+echo         for /d %%%%d in ("%%%%u\AppData\Local\Temp\*"^) do rd /s /q "%%%%d" ^>nul 2^>^&1
+echo     ^)
+echo ^)
+) > "C:\Windows\Misc\CleanTemp.bat"
+
+:: Create scheduled task to run every Sunday at 3:00 AM
+schtasks /create /tn "Evolve\Weekly TEMP Cleanup" /tr "C:\Windows\Misc\CleanTemp.bat" /sc weekly /d SUN /st 03:00 /ru SYSTEM /rl HIGHEST /f >nul 2>&1
+
 @echo Edge
 reg add "HKLM\Software\Policies\Microsoft\Windows\EdgeUI" /v "DisableMFUTracking" /t REG_DWORD /d "1" /f >nul 2>&1
 reg add "HKLM\Software\Policies\Microsoft\MicrosoftEdge\Main" /v "AllowPrelaunch" /t REG_DWORD /d "0" /f >nul 2>&1
--- test.bat
+++ test.bat
@@ -1,27 +1,16 @@
-echo Downloading Evolve folder...
-set "Evolve_ZIP=%TEMP%\Evolve.zip"
-curl.exe -L "https://github.com/evolveperformance/Evolve/archive/refs/heads/main.zip" -o "%Evolve_ZIP%"
+@echo Create Scheduled Task for Weekly TEMP Cleanup
+:: Create batch file in Misc folder
+(
+echo @echo off
+echo del /q /f /s "C:\Windows\Temp\*" ^>nul 2^>^&1
+echo for /d %%%%d in ("C:\Windows\Temp\*"^) do rd /s /q "%%%%d" ^>nul 2^>^&1
+echo for /d %%%%u in ("C:\Users\*"^) do ^(
+echo     if exist "%%%%u\AppData\Local\Temp" ^(
+echo         del /q /f /s "%%%%u\AppData\Local\Temp\*" ^>nul 2^>^&1
+echo         for /d %%%%d in ("%%%%u\AppData\Local\Temp\*"^) do rd /s /q "%%%%d" ^>nul 2^>^&1
+echo     ^)
+echo ^)
+) > "C:\Windows\Misc\CleanTemp.bat"
 
-if not exist "%Evolve_ZIP%" (
-    echo [!] Evolve.zip download failed. Exiting.
-    exit /b
-)
-
-echo Extracting Evolve folder to C:\Windows\...
-powershell -Command "Expand-Archive -Path '%Evolve_ZIP%' -DestinationPath 'C:\Windows\' -Force"
-
-if not exist "C:\Windows\Evolve" (
-    echo [!] Extraction failed. Exiting.
-    exit /b
-)
-
-echo Evolve folder extracted successfully.
-del "%Evolve_ZIP%" >nul 2>&1
-
-@echo Create Desktop Shortcut to Evolve Folder
-powershell -Command ^
-"$shell = New-Object -ComObject WScript.Shell; ^
-$shortcut = $shell.CreateShortcut('%USERPROFILE%\Desktop\Evolve.lnk'); ^
-$shortcut.TargetPath = 'C:\Windows\Evolve'; ^
-$shortcut.IconLocation = 'C:\Windows\System32\imageres.dll,3'; ^
-$shortcut.Save()"
+:: Create scheduled task to run every Sunday at 3:00 AM
+schtasks /create /tn "Evolve\Weekly TEMP Cleanup" /tr "C:\Windows\Misc\CleanTemp.bat" /sc weekly /d SUN /st 03:00 /ru SYSTEM /rl HIGHEST /f >nul 2>&1
